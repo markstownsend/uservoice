447d00e58391059164472876da236f625361c003
markstownsend <markstownsend@yahoo.com>
2015-08-31 00:31:59 +0100

    renaming and moving some things for the gist to work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/2f92825aa8a94515347a'));
 
 app.listen(process.env.PORT || 3898);
